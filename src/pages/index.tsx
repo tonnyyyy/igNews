1b1b81c2e3cff209176ca13d0456111271a57fa5
ti4 Gimi <tonyadryanogomes1@gmail.com>
2021-06-01 15:58:42 -0300

    nav bar style finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
- import Head from 'next/head'
- import styles from '../styles/global.scss'
+import Head from 'next/head'
+import { Header } from '../components/Header'
  export default function Home() {
   return (
     <>
       <Head>
         <title>ig.news | Home</title>
       </Head>
+
+      <main>
+        <section>
+          <span>👏 Hello, stranger!</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
       <h1>Test</h1>
     </>
   )

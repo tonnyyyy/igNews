2dc03e603f2c177a85ff98d7d2da4fac6c40d889
ti4 Gimi <tonyadryanogomes1@gmail.com>
2021-06-01 11:52:11 -0300

    upload project to github

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+ import Head from 'next/head'
+ import styles from '../styles/global.scss'
+ export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news | Home</title>
+      </Head>
+      <h1>Test</h1>
+    </>
+  )
+}

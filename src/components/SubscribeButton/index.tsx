ceed7334c3d913dc88f7725752b275af79d4b8de
ti4 Gimi <tonyadryanogomes1@gmail.com>
2021-08-02 13:51:40 -0300

    create checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,9 @@
 import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
 
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 interface SubscribeButtonProps {
   priceId: string;
 }
@@ -8,11 +11,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession(); // logado = true, deslogado = false
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github');
       return;
     }
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout( { sessionId });
+    }
+    catch (err) {
+      alert(err.message);
+    }
+
   }
 
   return (
